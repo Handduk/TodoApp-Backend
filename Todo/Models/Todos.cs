ed6d771fa1975025022d7c89faccb6300e4cf02f
handduk <martin.karlsson95@hotmail.com>
2023-09-14 16:13:03 +0200

    Added get command for api

---
+++ Todo/Models/Todos.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Models
+{
+    public class Todos
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
